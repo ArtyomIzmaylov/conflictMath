fa16d2c0daefc7d2de9365e549df1a214776d72f
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2023-05-16 22:09:08 +0300

    graph added

--- excelRead.py
+++ excelRead.py
@@ -2,12 +2,13 @@
 import pandas as pd
 
 # Укажите путь к вашему файлу Excel
-path = 'C:/Users/asus/PycharmProjects/numpa/table.xlsx'
+path = 'C:/Саморазвитие/Программирование/Python course/conflictMath/table.xlsx'
 
 df = pd.read_excel(path)
 print(df)
 matrix = df.iloc[:, 1:].values.astype(str).tolist()
-
+print(df['A'].tolist())
 print(matrix)
+
 
 
--- graph.py
+++ graph.py
@@ -1,1 +1,14 @@
-# -*- coding: utf-8 -*-
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+data = [0, '–', 0, '+', '+', '+', '–', '–', '–', 0, '+', '+']
+df = pd.DataFrame({'col': data}, index=range(1, len(data)+1))
+
+G = nx.Graph()
+
+# добавляем вершины в граф из индексов df
+G.add_nodes_from(df.index)
+labels = {i: str(i) + '(' + str(df.loc[i]['col']) + ')' for i in df.index}
+nx.draw(G, with_labels=True, labels=labels, font_size=16, node_size = 1500)
+plt.show()
+# рисуем граф
