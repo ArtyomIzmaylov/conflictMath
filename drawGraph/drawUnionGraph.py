0434b70e883a2583f9b1e1c467c2e15f7c2593eb
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2023-05-19 16:23:24 +0300

    clear

--- drawGraph/drawUnionGraph.py
+++ drawGraph/drawUnionGraph.py
@@ -1,47 +1,41 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
+
 import networkx as nx
 import matplotlib.pyplot as plt
-from readFile.excelRead import df as row
-from readFile.readText import symbol
 
 
-data = row[symbol].values.astype(str).tolist()
-data = [str(i).replace('0', '0') for i in data]
-data = [element.replace('–', '-') for element in data]
+def makeUnionGraph(df):
+    G = nx.Graph()
 
-df = pd.DataFrame({'col': data}, index=range(1, len(data)+1))
-G = nx.Graph()
+    # добавляем вершины в граф из индексов df
+    G.add_nodes_from(df.index)
 
-# добавляем вершины в граф из индексов df
-G.add_nodes_from(df.index)
-
-for node in G.nodes():
-    G.nodes[node]['color'] = '#A468D5'
+    for node in G.nodes():
+        G.nodes[node]['color'] = '#A468D5'
 
 
-for node1 in G.nodes():
-    for node2 in G.nodes():
-        if node1 != node2 and df.loc[node1, 'col'] == df.loc[node2, 'col']:
-            if df.loc[node1, 'col'] == '0':  # если значение в ячейке равно "0", добавляем пунктирное ребро
-                G.add_edge(node1, node2, color='#F30021', style='dashed')
-            if df.loc[node1, 'col'] == '+':  # если значение в ячейке равно "0", добавляем пунктирное ребро
-                G.add_edge(node1, node2, color='#F97083', style='dashed')
-            if df.loc[node1, 'col'] == '-':
-                G.add_edge(node1, node2, color='#7D0057', style='dashed')
+    for node1 in G.nodes():
+        for node2 in G.nodes():
+            if node1 != node2 and df.loc[node1, 'col'] == df.loc[node2, 'col']:
+                if df.loc[node1, 'col'] == '0':  # если значение в ячейке равно "0", добавляем пунктирное ребро
+                    G.add_edge(node1, node2, color='#F30021', style='dashed')
+                if df.loc[node1, 'col'] == '+':  # если значение в ячейке равно "0", добавляем пунктирное ребро
+                    G.add_edge(node1, node2, color='#F97083', style='dashed')
+                if df.loc[node1, 'col'] == '-':
+                    G.add_edge(node1, node2, color='#7D0057', style='dashed')
+    return G
 
-
-# рисуем граф
-pos = nx.circular_layout(G)
-labels = {i: str(i) + '(' + str(df.loc[i]['col']) + ')' for i in df.index}
-edges = G.edges()
-colors = [G[u][v]['color'] for u,v in edges]
-styles = [G[u][v]['style'] for u,v in edges]
-nx.draw(G, pos, labels=labels, with_labels=True, width = 3, font_size=16,
-        node_size=1500, edge_color=colors, style=styles,
-    edgecolors='black',
-        node_color=[G.nodes[n]['color'] for n in G.nodes()])
-plt.axis('on')
-plt.figtext(1, 0.01, 'Made by Artyom Gaibovich©', ha='right', va='bottom', size = 10)
-plt.savefig('../templates/my_plotUnion.png')
-plt.show()
+def drawUnionGraph(df, G):
+    pos = nx.circular_layout(G)
+    labels = {i: str(i) + '(' + str(df.loc[i]['col']) + ')' for i in df.index}
+    edges = G.edges()
+    colors = [G[u][v]['color'] for u,v in edges]
+    styles = [G[u][v]['style'] for u,v in edges]
+    nx.draw(G, pos, labels=labels, with_labels=True, width = 3, font_size=16,
+            node_size=1500, edge_color=colors, style=styles,
+        edgecolors='black',
+            node_color=[G.nodes[n]['color'] for n in G.nodes()])
+    plt.axis('on')
+    plt.figtext(1, 0.01, 'Made by Artyom Gaibovich©', ha='right', va='bottom', size = 10)
+    plt.savefig('./templates/my_plotUnion.png')
+    plt.show()
--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from readFile.excelRead import df as row
 from readFile.readText import symbol
 from makeTable.makeTable import makeTable
 from drawGraph.drawGraphs import makeGraph, drawGraph
-
+from drawGraph.drawUnionGraph import makeUnionGraph, drawUnionGraph
 symbol = symbol
 data = row[symbol].values.astype(str).tolist()
-data = [str(i).replace('0', '0') for i in data]
+data = [str(i).replace('0'  , '0') for i in data]
 data = [element.replace('–', '-') for element in data]
 df = pd.DataFrame({'col': data}, index=range(1, len(data)+1))
 
 
+
 makeTable()
+drawUnionGraph(df, makeUnionGraph(df))
 drawGraph(df, makeGraph('+', df), '+', 'Positive')
 drawGraph(df, makeGraph('-', df), '-', 'Negative')
 drawGraph(df, makeGraph('0', df), '0', 'Neutral')
-input('Нажмите Enter для выхода\n')
